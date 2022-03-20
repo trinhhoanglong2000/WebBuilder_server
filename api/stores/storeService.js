173c821e280fb9d67bb7d80ab819226641fcc7a3
nhaatjminh <minhphan.240199@gmail.com>
2022-03-20 20:58:12 +0700

    fix bug

--- api/stores/storeService.js
+++ api/stores/storeService.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Store = require('./storeModel');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
 
 exports.createStore = (storeObj) => {
     try {
