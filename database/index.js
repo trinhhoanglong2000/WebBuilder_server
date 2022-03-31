cb237ab50e56dacf20de8116e5ad78824da1115c
nhaatjminh <minhphan.240199@gmail.com>
2022-03-31 22:46:14 +0700

    create pg pool

---
+++ database/index.js
@@ -0,0 +1,9 @@
+const Pool = require("pg").Pool
+const poolean = new Pool({
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: 5432
+})
+module.exports = poolean
