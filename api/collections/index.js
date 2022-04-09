5e0e390e3f2414dabfd97a46583abbbb4a381b5d
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-04-10 03:11:47 +0700

    Done Demo Carousel

--- api/collections/index.js
+++ api/collections/index.js
@@ -3,27 +3,18 @@ const router = express.Router();
 const productcollectionController = require('./productcollections/productcollectionController')
 const bannercollectionController = require('./bannercollections/bannercollectionController')
 const authenticator = require('../../middleware/authentication')
-
-<<<<<<< HEAD
-
 //#region carousel
 const carouselsController = require('./carousel/carouselsController')
 router.get('/carousels',carouselsController.getAllCollections)
 router.get('/category/:id',carouselsController.getCategoryData)
 //#endregion
 
-
-router.get('/', collectionController.getAllCollections);
-router.get('/:id', collectionController.getcollectionById);
-router.get('/:id/banners', collectionController.getBannersByCollectionId);
-=======
 router.get('/product', productcollectionController.getAllCollections);
 router.get('/product/:id', productcollectionController.getcollectionById);
 
 router.get('/banner', bannercollectionController.getAllCollections);
 router.get('/banner/:id', bannercollectionController.getcollectionById);
 // router.get('/:id/banners', collectionController.getBannersByCollectionId);
->>>>>>> abe80d492376644c7ca1c555d8d53c4691566c2b
 
 
 /* POST create account. */
--- dist/js/template-default/carousel/carousel.js
+++ dist/js/template-default/carousel/carousel.js
@@ -1,6 +1,5 @@
 carousel_block_js = true;
-debugger
-alert('Carousel are loaded');
+//alert('Carousel are loaded');
 console.log(document)
 function callback(mutationList) {
   mutationList.forEach(function (mutation) {
@@ -8,7 +7,11 @@ function callback(mutationList) {
       case "attributes":
         switch (mutation.attributeName) {
           case "data":
-            console.log(mutation.target.getAttribute("data"));
+            let carouselIndicators = $(`#${mutation.target.getAttribute("id")} .carousel-indicators`)[0]
+            let carouselInner = $(`#${mutation.target.getAttribute("id")} .carousel-inner`)[0];
+            carouselIndicators.innerHTML  ="";
+            carouselInner.innerHTML = "";
+            CarouselsGenerateCodeItem(mutation.target.getAttribute("data"),mutation.target.getAttribute("id"))
             break
         }
         break;
@@ -27,39 +30,46 @@ function SetListenOnChangeAtrribute() {
     });
   }
 }
-function GetData() {
 
-}
-async function GenerateCode() {
-  let carouselListElement = $("div[ez-mall-type='carousel']")
-  for (let i = 0; i < carouselListElement.length; i++) {
-    let categoryId = carouselListElement[i].getAttribute("data");
-    const response = await fetch(`http://localhost:5000/collections/category/${categoryId}`
-      , {
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      });
-    let myJson = await response.json();
-    debugger
-    let carouselIndicators = $("div[ez-mall-type='carousel'] .carousel-indicators")[0]
-    let htmlButtonInsert = `<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 4"></button>`
+async function CarouselsGenerateCodeItem(categoryId, itemID) {
+  const response = await fetch(`http://localhost:5000/collections/category/${categoryId}`
+    , {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  let myJson = await response.json();
+  let data = myJson.data;
+  console.log(myJson)
+  let carouselIndicators = $(`#${itemID} .carousel-indicators`)[0]
+  let carouselInner = $(`#${itemID} .carousel-inner`)[0];
+  data.forEach((item, index) => {
+    let htmlButtonInsert = `
+  <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="${index}" class = "${index == 0 ? "active" : ""}" aria-label="Slide ${index}"></button>
+  `
     carouselIndicators.insertAdjacentHTML("beforeend", htmlButtonInsert);
 
-    let carouselInner = $("div[ez-mall-type='carousel'] .carousel-inner")[0];
     let htmlCarouselItemInsert = `
-    <div class="carousel-item">
-      <img src="https://dummyimage.com/1360x540/55595c/fff " class="d-block w-100" alt="https://dummyimage.com/1360x540/55595c/fff ">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>NEW slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+  <div class="carousel-item ${index == 0 ? "active" : ""}">
+    <img src="${item.image}" class="d-block w-100" alt="${item.image}">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>${item.caption}</h5>
+      <p>${item.description}</p>
     </div>
-  `
+  </div>
+`
     carouselInner.insertAdjacentHTML("beforeend", htmlCarouselItemInsert)
+  })
+}
+function CarouselsGenerateCodeStart() {
+  let carouselListElement = $(`div[ez-mall-type='carousel']`)
+  for (let i = 0; i < carouselListElement.length; i++) {
+    let categoryId = carouselListElement[i].getAttribute("data");
+    let itemID = carouselListElement[i].getAttribute("id");
+    CarouselsGenerateCodeItem(categoryId,itemID)
   }
 }
-GenerateCode();
-SetListenOnChangeAtrribute();
+CarouselsGenerateCodeStart();
+//SetListenOnChangeAtrribute();
 
