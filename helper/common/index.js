cac21768a0774db62a3bd246bc74e399ec647c42
nhaatjminh <minhphan.240199@gmail.com>
2022-05-22 01:03:54 +0700

    fix regex

--- helper/common/index.js
+++ helper/common/index.js
@@ -1,4 +1,4 @@
 
 exports.generateURL = (s) => {
-    return s.replace(/\s+/g,' ').trim().toLowerCase().replace(' ', '-');
+    return  s.replace(/\s+/g,' ').trim().toLowerCase().replace(/ /g, '-');
 }
