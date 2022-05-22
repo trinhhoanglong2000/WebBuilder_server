d522b8ada38290ffe9014beb45204207809dc312
DESKTOP-RRM9HUR\MSI <ttlgame123@gmail.com>
2022-05-22 15:23:35 +0700

    Heroku

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(passport.initialize());
+app.use(express.static('dist'))
 
 app.use('*',userStoreRouter)
 
