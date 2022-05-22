96c62ea6c72394e9b5d6aa0f30d0f4b44086fc3d
DESKTOP-RRM9HUR\MSI <ttlgame123@gmail.com>
2022-05-22 15:03:48 +0700

    Heroku

--- api/files/fileController.js
+++ api/files/fileController.js
@@ -8,18 +8,15 @@ exports.getFileName = async (req, res) =>{
   
     let productionPath = path.join(path.dirname(require.main.filename),'../')
     console.log("Hi")
-    if (productionPath === "/app/"){
-        productionPath = '/'
-    }
+
     var options = {
         root: productionPath
     }
-    console.log(path.join(path.dirname(require.main.filename),'../'))
-    
-    res.status(http.Success).sendFile(`${req.params.filename}`,options)
-   
-    // var fileName = `${req.params.filename}`;
-    // res.sendFile(fileName, options);
+
+    res.sendFile(`${req.params.filename}`, {
+        root: path.join(__dirname ,`../../`)
+    })
+
    
 }
 
