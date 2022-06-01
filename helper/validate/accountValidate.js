357aa77d19921c73ca8eaae9ee47230ae04128c4
nhaatjminh <minhphan.240199@gmail.com>
2022-06-01 22:34:32 +0700

    fix validate pw when sign up with social

--- helper/validate/accountValidate.js
+++ helper/validate/accountValidate.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 exports.validateAccount = (accountObj) => {
     var schema = Joi.object({
         email: Joi.string().email({ minDomainSegments: 2}).required(),
-        password: Joi.string().min(8).max(30).required(),
+        password: (accountObj.gg_id || accountObj.fb_id) ? Joi.any() : Joi.string().min(8).max(30).required(),
         fullname: Joi.string().required(),
         phone: Joi.string().alphanum().length(10),
         gender: Joi.boolean(),
