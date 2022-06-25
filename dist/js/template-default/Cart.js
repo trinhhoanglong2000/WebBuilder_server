c47342835c23149bd720f26cfe68a115e2f08084
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-06-25 21:10:12 +0700

    Commit submit cart

--- dist/js/template-default/Cart.js
+++ dist/js/template-default/Cart.js
@@ -1,9 +1,27 @@
 function payMent(){
-    let productBuy = [];
+    let listIdCheck = [];
+    let listProductBuy= [];
     let productCheck = $(".ezMall-cart-item");
     if(productCheck.length > 0){
         for(let i =0 ;i < productCheck.length; i++){
-            console.log(productCheck[i]);
+            let isCheckked = $(productCheck[i]).find("input.ezMall-cart-item-check:checked");
+            if(isCheckked.length!=0){
+                listIdCheck.push(isCheckked[i].id)
+            }
         }
+        console.log(listIdCheck)
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        for(let i =0; i < cart.length;i++){
+            if(cart[i].is_variant){
+                if(listIdCheck.includes( cart[i].id)){
+                    listProductBuy.push(cart[i])
+                }
+            }else{
+                if(listIdCheck.includes( cart[i].variant_id)){
+                    listProductBuy.push(cart[i])
+                }
+            }
+        }
+        console.log(productCheck)
     }
 }
