e775d23b5bf2169cf2d7d99e03fdb2cb73023cb2
DESKTOP-RRM9HUR\MSI <ttlgame123@gmail.com>
2022-08-01 00:11:14 +0700

    Fix CORS

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 
 var hbs = require('express-handlebars')
 const corsOptions = {
-  origin: true,
+  origin: false,
   optionsSuccessStatus: 200,
   credentials: true,
   methods: "GET, PUT, POST, DELETE"
@@ -73,7 +73,7 @@ app.use('/order', orderRouter)
 app.use('/data',dataRouter)
 app.use('/discount',discountRouter)
 app.get('/',function (req,res) {
-  res.send("Hi Ver 1.0")
+  res.send("Hi Ver 1.1")
 })
 app.use('/menu', menuRouter);
 app.use('/menu-item', menuItemRouter);
