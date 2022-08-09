196ca3506a8b5bd795aff34f571ea5eec54d2854
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-08-10 00:15:17 +0700

    Copy file to another template

--- dist/js/Pet Shop/Payment.js
+++ dist/js/Pet Shop/Payment.js
@@ -73,6 +73,7 @@ function buy() {
     $(rootEle).find(".ezMall-payment-alert .ezMall-loading").show();
     let storeId = $(rootEle).attr("ez-mall-store");
     let email = $(rootEle).find("#email").val();
+    let node =   $(rootEle).find("#note").val();
     if (!email) {
         $(rootEle).find(".email-alert").show();
         isOmitFill = true;
@@ -115,6 +116,7 @@ function buy() {
     let currency = payment == 1 ? "USD" : $(rootEle).find("#currency").val();
     let discountInfo = JSON.parse(localStorage.getItem('discount'));
 
+
     var payload = {
         order: {
             email: email,
@@ -128,7 +130,7 @@ function buy() {
             payment_method: payment,
             currency: currency,
             discount_id: discountInfo ? discountInfo.id : '',
-            note: ''
+            note: node??''
         },
         products: paymentItems.map((item) => {
             return {
@@ -229,7 +231,7 @@ async function getDistrict(rootEle) {
     cityOptions[indexCity].data.forEach(item => {
         const rowHtml =
             `                            
-                <option value="${item.id}" >${item.name}</option>        
+                <option value="${item.name}" >${item.name}</option>        
             `
         paymentDistrictSelectContainer.insertAdjacentHTML("beforeend", rowHtml);
     })
@@ -417,13 +419,15 @@ function useDiscount() {
         })
         .then(function (res) { return res.json(); })
         .then(function (resData) {
-            if (resData.statusCode == 200 && data) {
+            if (resData.statusCode == 200 && resData.data) {
                 window.localStorage.setItem('discount', JSON.stringify(resData.data[0]));
+                $(rootEle).find("#incorrect-discount-alert").hide();
                 $(rootEle).find(".ezMall-discount-description").show();
-                let discount_text = $(rootEle).find(".ezMall-discount-description-text").html(resData.data[0].code);
+                $(rootEle).find(".ezMall-discount-description-text").html(resData.data[0].code);
             } else {
                 window.localStorage.removeItem('discount');
                 $(rootEle).find(".ezMall-discount-description").hide();
+                $(rootEle).find("#incorrect-discount-alert").show();
             }
         }).finally(() => {
             loadPaymentData(rootEle, false)
--- dist/js/Shopping Template/Cart.js
+++ dist/js/Shopping Template/Cart.js
@@ -28,15 +28,21 @@ function payMent() {
             }
         }
         window.localStorage.setItem('paymentItems', JSON.stringify(listProductBuy));
-        window.location.href = "/payment"
+        if(listProductBuy.length > 0){
+            window.location.href = "/payment"
+        }else{
+            $("#cart-alert").html("Please choose one or more items")
+        }
+
     }
 }
 
 async function CartGenerateCodeItem(e) {
-    
     let rootEle = $(e)
-    let tableHead = $(e).find(`table thead`)[0];
-    let tableBody = $(e).find(`table tbody`)[0];
+    $(rootEle).find(".ezMall-popup-alert").show().css("display", "flex").children().hide()
+    $(rootEle).find(".ezMall-popup-alert .ezMall-loading").show();
+    let tableHead = $(e).find(`.tableRoot .thead`)[0];
+    let tableBody = $(e).find(`.tableRoot .tbody`)[0];
     let ezMallSumary = $(e).find(`.ezMallSumary`)[0];
     let buttonGoShopping = $('#ezMall-cart-zero-item button').click(() => {
         window.location.href = "/collections"
@@ -75,6 +81,7 @@ async function CartGenerateCodeItem(e) {
         cart = []
     }
     insertCartData(cart, tableHead, tableBody, ezMallSumary, rootEle)
+    $(rootEle).find(".ezMall-popup-alert").hide().children().hide();
 }
 function CartGenerateCodeStart() {
     $("div[ez-mall-type='cart']").each(function (i) {
@@ -102,14 +109,17 @@ function calculateTotal(tableBody, tableHead, ezMallSumary, rootEle) {
     let checkedInput = $(tableBody).find(".ezMall-cart-item .ezMall-cart-item-check:checked")
     if (items.length == 0) {
         $(rootEle).find("#ezMall-cart-zero-item").show().addClass("d-flex");
-        $(rootEle).find("table").hide();
+        $(rootEle).find(".cart-container").hide();
         $(ezMallSumary).hide();
         $(tableHead).find("#cart-select-all-product").prop("checked", false)
     }
     else {
         $(rootEle).find("#ezMall-cart-zero-item").hide().removeClass("d-flex");
-        $(rootEle).find("table").show();
+        $(rootEle).find(".cart-container").show();
         $(ezMallSumary).show();
+        if(checkedInput.length != 0 ){
+            $("#cart-alert").html("")
+        }
         if (checkedInput.length == items.length) {
             $(tableHead).find("#cart-select-all-product").prop("checked", true)
         }
@@ -146,24 +156,24 @@ function calculateTotal(tableBody, tableHead, ezMallSumary, rootEle) {
 }
 function insertCartData(data, tableHead, tableBody, ezMallSumary, rootEle) {
     let currency = JSON.parse(localStorage.getItem('storeCurrency'));
-    $(ezMallSumary).find("#ezMall-cart-sumary-unchecked-all").click(() => {
+    $(rootEle).find(".ezMall-cart-sumary-unchecked-all").click(() => {
         let checkedInput = $(tableBody).find(".ezMall-cart-item .ezMall-cart-item-check:checked ")
         for (let i = 0; i < checkedInput.length; i++) {
             $(checkedInput[i]).prop("checked", false);
         }
         calculateTotal(tableBody, tableHead, ezMallSumary, rootEle);
     })
-    $(ezMallSumary).find(".ezMallSumary-total-cost").html(priceToString(0, currency));
+    $(rootEle).find(".ezMallSumary-total-cost").html(priceToString(0, currency));
 
-    $(tableHead).find(".ezMall-head-remove-all-items").click(() => {
+    $(rootEle).find(".ezMall-head-remove-all-items").click(() => {
         window.localStorage.setItem('cart', JSON.stringify([]));
         $(tableBody).html("")
-        $(tableHead).find("#cart-select-all-product").prop('checked', false);
+        $(rootEle).find("#cart-select-all-product").prop('checked', false);
         calculateTotal(tableBody, tableHead, ezMallSumary, rootEle);
         updateCart()
     });
 
-    $(tableHead).find("#cart-select-all-product").click((e) => {
+    $(rootEle).find("#cart-select-all-product").click((e) => {
         var checkBox = $(tableBody).find(".ezMall-cart-item .ezMall-cart-item-check");
         for (let i = 0; i < checkBox.length; i++) {
             $(checkBox[i]).prop('checked', e.target.checked)
@@ -180,59 +190,59 @@ function insertCartData(data, tableHead, tableBody, ezMallSumary, rootEle) {
             totalCostInit += totalPrice;
             let id = element.is_variant ? element.variant_id : element.id;
             const rowHtml =
-                `
-            <tr id  = ${id} class= "ezMall-cart-item" >
-                <th class="name">
-                    <div class="form-check">
-                        <div class="row">
-                            <div class="col-auto d-flex align-items-center">
-                                <input class="form-check-input ezMall-cart-item-check" type="checkbox" id=${"check-" + id} name=${"check-" + id} value="">
-                            </div>
-                            <div class="col-md-11 col-8">
-                                <a class="row ezMall-item-link" href="/products?id=${element.id}">
-           
-                                    <div class="col-xl-4 row d-flex justify-content-center">
-                                        <img src=${element.thumnail} alt="Image"
-                                            style="height: 150px;width: auto;">
-                                    </div>
-                                    <div class="col-xl-8 row d-flex flex-column justify-content-center">
-                                    
-                                        <div class="p-0 justify-content-center text-center my-3"> ${element.product_name} ${element.is_variant ? ` - ${element.variant_name}` : ""} </div>
-                                        <div class="p-0 justify-content-center text-center eZmall-for-des"></div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </th>
-                <td class="price">
-                    <div class=" d-flex justify-content-center align-items-center " style="height:150px">
-                    <div class="ezMall-item-price px-1"> ${priceToString(element.price, element.currency)} </div>    
-                    <div class= "ezMall-item-price-type">
-                    ${element.currency}
-                    </div>
-                    
-                </td>
-                <td class="quantity">
-                    <div class=" d-flex justify-content-center align-items-center" style="height:150px">
-                        <input type="number" min="1" id=${"val-" + id} class="form-control ezMall-item-quantity" value=${element.quantity}
-                            style="min-width: 70px; width: 70px;" />
-    
-                    </div>
-                </td>
-                <td class="ezMall-item-total justify-content-center align-items-center">
-                    <div class="d-flex justify-content-center align-items-center" style="height:150px">${priceToString(totalPrice, element.currency)}</div>
-                </td>
-                <th scope="col">
-                    <div class="d-flex justify-content-center align-items-center" style="height:150px">
-                        <button type="button"  class="ezMall-cart-item-delete btn fa fa-trash" data-toggle="button" aria-pressed="false"
-                            autocomplete="off" style="height: 29px;padding: 0px 10px;">
-                        </button>
-                    </div>
-                </th>
-            </tr>                           
-                                    
             `
+        <div id  = ${id} class= "ezMall-cart-item row py-1 px-3" >
+            <div class="name col-md-9">
+                    <div class="row">
+                                <div class="col-md-4 col-7 row d-flex justify-content-start p-0">
+                                        <a href="/products?id=${id}">
+                                            <img src=${element.thumnail} alt="Image"
+                                            style="height: 150px;width: 100%;">
+                                        </a>
+                                    </div>
+                                    <div class="col-md-8 col-5 d-flex flex-column justify-content-between px-3">
+                                        <div class="px-0 py-0 my-2 justify-content-start  text-start fw-bold cart-item-tittle">
+                                            <a href="/products?id=${id}"> ${element.product_name} ${element.is_variant? ` - ${element.variant_name}`: ""}</a>
+                                        </div>
+                                        <div class = "d-flex justify-content-between px-0 ">
+                                                <div class= "p-0 my-2 fw-bold d-flex text-secondary align-items-center fst-italic">
+                                                    Price: 
+                                                    <div class="ezMall-item-price px-1"> 
+                                                        ${priceToString(element.price, element.currency)}
+                                                    </div>    
+                                                    <div class= "ezMall-item-price-type">
+                                                        ${element.currency}
+                                                    </div>
+                                                </div>
+                                        </div>
+                                        <div class="fw-bold d-flex text-secondary align-items-center px-0 fst-italic">
+                                            <input type="number" min="0" id=${"val-" + id} class="form-control ezMall-item-quantity" value=${element.quantity}
+                                            style="min-width: 135px; width: 135px;" min=1>
+                                        </div>
+                                    </div>
+                    </div>
+            </div>
+            <div class="ezMall-item-total col-md-3 d-flex align-items-md-end align-items-center flex-md-column flex-row-reverse justify-content-between px-2">
+                <div class="my-2 d-flex align-items-center">
+                    <label class="form-check-label px-2 " for=${"check-" + id}>
+                    Take it
+                    </label>
+                    <input class="form-check-input ezMall-cart-item-check" type="checkbox" id=${"check-" + id} name=${"check-" + id} value="">
+                </div>
+                <div class="px-1 py-0 my-2 justify-content-end  text-start fw-bold cart-item-tittle color-orange">
+                    ${priceToString(totalPrice, element.currency)}
+                </div>
+                <div class="d-flex justify-content-end align-items-center">
+                    <button type="button"  class="ezMall-cart-item-delete btn ezMall-btn" data-toggle="button" aria-pressed="false"
+                        autocomplete="off" style="height: 29px;padding: 0px 10px;">
+                        <i class="fa fa-trash " aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        </div>     
+        <hr class = "my-2">                      
+                                
+        `
 
             tableBody.insertAdjacentHTML("beforeend", rowHtml);
             $(tableBody).find(`#${id} button.ezMall-cart-item-delete`).click(() => {
@@ -282,12 +292,27 @@ function insertCartData(data, tableHead, tableBody, ezMallSumary, rootEle) {
                     }
                     return false;
                 })
-                let currentQuantity = $(tableBody).find(`input#val-${id}`).val()
+                let currentQuantity = $(tableBody).find(`input#val-${id}`).val();
+
+                if(!currentQuantity.toString().match(/^[1-9]\d*$/)){
+                    $('.ezMallSumary button').attr('disabled','disabled');
+                    $(`#${id} input.ezMall-item-quantity`).css("border-color", "red").css("background","#ffdddd")  
+                    $(`#${id} input.ezMall-item-quantity`).attr('invalid', 'true') 
+                    return
+                }else{
+                    $(`#${id} input.ezMall-item-quantity`).css("border-color", "#ced4da").css("background","#fff")  
+                    $(`#${id} input.ezMall-item-quantity`).removeAttr('invalid')
+                    if($(`input.ezMall-item-quantity[invalid='true']`).length == 0){
+                        $('.ezMallSumary button').removeAttr('disabled');
+                    }
+
+                }
+
                 if(currentQuantity <=0){
                     $(tableBody).find(`#${id} button.ezMall-cart-item-delete`).click();
                     return
                 }
-                $(tableBody).find(`#${id} .ezMall-item-total div`).html((Number)(element.price) * currentQuantity + ` ${element.currency}`)
+                $(tableBody).find(`#${id} .ezMall-item-total .cart-item-tittle`).html( priceToString((Number)(element.price) * currentQuantity, element.currency))
                 let items = $(tableBody).find(".ezMall-cart-item ");
                 let totalCost = 0;
                 calculateTotal(tableBody, tableHead, ezMallSumary, rootEle)
@@ -305,13 +330,13 @@ function insertCartData(data, tableHead, tableBody, ezMallSumary, rootEle) {
     let items = $(tableBody).find(".ezMall-cart-item ");
     if (items.length == 0) {
         $(rootEle).find("#ezMall-cart-zero-item").show().addClass("d-flex");
-        $(rootEle).find("table").hide();
+        $(rootEle).find(".cart-container").hide();
         $(ezMallSumary).hide();
         $(tableHead).find("#cart-select-all-product").prop("checked", false)
     }
     else {
         $(rootEle).find("#ezMall-cart-zero-item").hide().removeClass("d-flex");
-        $(rootEle).find("table").show();
+        $(rootEle).find(".cart-container").show();
         $(ezMallSumary).show();
     }
 }
