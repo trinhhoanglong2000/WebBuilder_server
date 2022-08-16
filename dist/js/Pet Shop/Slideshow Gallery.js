ddeb7c1efd14f5a55c5debc483d087f93194d7db
18127168 <tranbui.tainhan1002@gmail.com>
2022-08-17 00:11:14 +0700

    Fix bug Slideshow component

--- dist/js/Pet Shop/Slideshow Gallery.js
+++ dist/js/Pet Shop/Slideshow Gallery.js
@@ -1,35 +1,53 @@
-$(document).find('[name="slideshowGallery"]').each(function() {
-    const curSlide = $(this).find('.mySlides img');
-    const optionImage = $(this).find('img.option-image');
-    const prevBtn= $(this).find('a.prev');
-    const nextBtn = $(this).find('a.next');
+function embedSlideShowGalarryData(deploy) {
+    if (deploy) {
+        $('[name="slideshowGallery"]').each(function() {
+            const curSlide = $(this).find('.mySlides img');
+            const optionImage = $(this).find('div:not(.d-none) > img.option-image');
+            const prevBtn= $(this).find('a.prev');
+            const nextBtn = $(this).find('a.next');
+            debugger
+            optionImage.on("click", function() {
+                optionImage.each(function() { $(this).removeClass('active'); })
+                $(this).addClass('active');
+                $(curSlide).attr('src', $(this).attr('src'))
+            })
+    
+            prevBtn.on("click", () => {
+                const active = $(this).find('img.option-image.active')
+                const index = $(active).parent().closest('div').index() - 1;
+                if (index > -1 && index < optionImage.length) { 
+                    $(active).removeClass('active');
+                    $(optionImage[index]).addClass('active');
+    
+                    $(curSlide).attr('src', $(optionImage[index]).attr('src'))
+                }
+            })
+    
+            nextBtn.on("click", () => {
+                const active = $(this).find('img.option-image.active');
+                const index = $(active).parent().closest('div').index() + 1;
+                
+                if (index > -1 && index < optionImage.length) { 
+                    $(active).removeClass('active');
+                    $(optionImage[index]).addClass('active');
+    
+                    $(curSlide).attr('src', $(optionImage[index]).attr('src'))
+                }
+            })
 
-    optionImage.on("click", function() {
-        optionImage.each(function() { $(this).removeClass('active'); })
-        $(this).addClass('active');
-        $(curSlide).attr('src', $(this).attr('src'))
-    })
+            const active = $(this).find('img.option-image.active');
+            $(curSlide).attr('src', $(active).attr('src'))
+        });
+    }
+}
 
-    prevBtn.on("click", () => {
-        const active = $(this).find('img.option-image.active')
-        const index = $(active).parent().closest('div').index() - 1;
-        if (index > -1 && index < optionImage.length) { 
-            $(active).removeClass('active');
-            $(optionImage[index]).addClass('active');
-
-            $(curSlide).attr('src', $(optionImage[index]).attr('src'))
-        }
-    })
-
-    nextBtn.on("click", () => {
-        const active = $(this).find('img.option-image.active')
-        const index = $(active).parent().closest('div').index() + 1;
-        
-        if (index > -1 && index < optionImage.length) { 
-            $(active).removeClass('active');
-            $(optionImage[index]).addClass('active');
-
-            $(curSlide).attr('src', $(optionImage[index]).attr('src'))
-        }
-    })
-});
+$(document).ready(function () {
+    if ($('[data-gjs-type="wrapper"]').length) {
+        $('[data-gjs-type="wrapper"]').ready(function () {
+            embedSlideShowGalarryData(false);
+        })
+    }
+    else {
+        embedSlideShowGalarryData(true);
+    } 
+})
